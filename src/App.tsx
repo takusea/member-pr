bc852be8b918f53b90fc7f4cc89de076009ccfd0
takusea <takusea826@gmail.com>
2023-12-22 02:26:51 +0900

    グローバルスタイルを設定

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
 	return (
 		<>
-			<div>
+			<div className="container">
 				<Header step="1">a</Header>
 				<Card>
 					<p className="desc">
