010dacabb2abcb55ca586129fc4e8829c527e40f
takusea <takusea826@gmail.com>
2023-12-31 14:03:02 +0900

    スタイルを更新

--- src/components/card_contents/CardContent.css.ts
+++ src/components/card_contents/CardContent.css.ts
@@ -1,15 +1,32 @@
 import { style } from "@vanilla-extract/css";
 
 export const styles = {
+	section: style({
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "start",
+		marginBottom: "2rem",
+	}),
+	section_center: style({
+		alignItems: "center",
+		textAlign: "center",
+	}),
 	headline: style({
 		fontWeight: 700,
 		fontSize: "2rem",
 		marginBottom: "1rem",
 	}),
 	desc: style({
-		fontSize: "1rem",
+		fontSize: "1.125rem",
 		lineHeight: 1.75,
 		marginBottom: "1rem",
+	}),
+	desc_icon: style({
+		marginRight: 8,
+	}),
+	img: style({
+		display: "block",
+		margin: "auto",
 	}),
 	subheadline: style({
 		fontSize: "1rem",
@@ -21,5 +38,13 @@ export const styles = {
 		paddingLeft: 16,
 		paddingRight: 16,
 		marginBottom: "1rem",
+	}),
+	column_2: style({
+		display: "flex",
+		gap: 32,
+	}),
+	timer: style({
+		fontSize: "3rem",
+		height: 64,
 	}),
 };
