432887c37b7c0d4b1798d3e5b0a303390cd86e05
takusea <takusea826@gmail.com>
2023-12-31 12:46:57 +0900

    App.tsxを更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import "./App.css";
-import Card from "./components/Card";
-import Header from "./components/Header";
+import Header from "./components/base/Header";
+import Card from "./components/base/Card";
+
+import Step1_1 from "./components/card_contents/Step1_1";
+import Step1_2 from "./components/card_contents/Step1_2";
+import Step1_3 from "./components/card_contents/Step1_3";
+import Step1_4 from "./components/card_contents/Step1_4";
+import { useState } from "react";
 
 function App() {
+	const [finished, setFinished] = useState(false);
+
 	return (
-		<>
-			<div className="container">
-				<Header step="1">a</Header>
-				<Card>
-					<p className="desc">
-						2種類のカードを使って才能をみつけるための対話を行ないます！
-					</p>
-					<p className="desc">
-						1人ずつ答える役と深ぼる役になり、対話の順番を回していきましょう。
-					</p>
-				</Card>
-			</div>
-		</>
+		<div className="container">
+			<Header step="1">カードを使って対話をする</Header>
+			<Card finished={finished} onFinishButtonClick={() => {}}>
+				<Step1_1 />
+				<Step1_2 />
+				<Step1_3 />
+				<Step1_4 onExpire={() => setFinished(true)} />
+			</Card>
+		</div>
 	);
 }
 
